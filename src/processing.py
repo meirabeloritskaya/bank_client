092bd8a05fc468a8ae86328e1f2e7a7aa45c6340
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-05-09 00:27:41 +0300

    add function select_state_id

---
+++ src/processing.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+
+def select_state_id(list_state_id: list, state: str = "EXECUTED") -> list:
+    """функция возвращет список id по заданным state"""
+    list_id = list(filter(lambda el: el["state"] == state, list_state_id))
+    return list_id
+
+
+list_state_id = [
+    {"id": 1, "state": "EXECUTED", "date": "2021-07-03"},
+    {"id": 2, "state": "CANCELED", "date": "2014-07-03"},
+    {"id": 3, "state": "EXECUTED", "date": "2019-01-03"},
+    {"id": 4, "state": "CANCELED", "date": "2010-07-03"},
+]
+
+
+new_list_id = select_state_id(list_state_id, "CANCELED")
+print(new_list_id)
