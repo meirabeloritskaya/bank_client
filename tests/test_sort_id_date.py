2247c6edfe4829ed8fc820334fbe05a73b1affcd
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-05-12 15:20:29 +0300

    add test_sort_id_date

---
+++ tests/test_sort_id_date.py
@@ -0,0 +1,33 @@
+import pytest
+from src.processing import sort_id_date
+
+
+@pytest.fixture
+def id_state_date():
+    return [
+        {"id": 5, "state": "EXECUTED", "date": "2021-07-03"},
+        {"id": 7, "state": "EXECUTED", "date": "2019-01-02"},
+    ]
+
+
+@pytest.mark.parametrize(
+    "revers, expected",
+    [
+        (
+            "True",
+            [
+                {"id": 5, "state": "EXECUTED", "date": "2021-07-03"},
+                {"id": 7, "state": "EXECUTED", "date": "2019-01-02"},
+            ],
+        ),
+        (
+            "False",
+            [
+                {"id": 7, "state": "EXECUTED", "date": "2019-01-02"},
+                {"id": 5, "state": "EXECUTED", "date": "2021-07-03"},
+            ],
+        ),
+    ],
+)
+def test_sort_id_date(id_state_date, revers, expected):
+    assert sort_id_date(id_state_date, revers) == expected
