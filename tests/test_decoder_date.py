e48860aa179c73d87de84636fd845cb8f2b281d5
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-05-14 11:02:42 +0300

    fix from src import masks

--- tests/test_decoder_date.py
+++ tests/test_decoder_date.py
@@ -4,13 +4,6 @@ import pytest
 from src.widget import decoder_date
 
 
-# from unittest.mock import MagicMock, patch
-#
-# # Создаем заглушку (mock) для модуля masks
-# masks = MagicMock()
-#
-# # Устанавливаем заглушку вместо реального модуля
-# with patch('src.widget.masks', masks):
 @pytest.mark.parametrize(
     "cod_date, expected", [("2018-07-11T02:26:18.671407", "11.07.2018")]
 )
