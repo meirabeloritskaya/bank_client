48ce18750264975a32c10a3da4081dfe319485ad
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-06-07 02:44:53 +0300

    add external_api

---
+++ src/external_api.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import os
+from utils import get_data_transactions
+
+from dotenv import load_dotenv
+
+load_dotenv("C:/Users/Meira/PycharmProjects/card_client/.env")
+
+path = "C:/Users/Meira/PycharmProjects/card_client/data/operations.json"
+
+
+def amount_transaction(transaction_by_id):
+    """функция возвращает сумму транзакции в рублях"""
+
+    trans_amount = transaction_by_id["operationAmount"]["amount"]
+    trans_code = transaction_by_id["operationAmount"]["currency"]["code"]
+    api_key = os.getenv("API_KEY")
+    headers_api = {"apikey": api_key}
+    if trans_code == "RUB":
+        return trans_amount
+    else:
+        try:
+
+            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={trans_code}&amount={trans_amount}"
+            response = requests.request("GET", url, headers=headers_api)
+            my_result = response.json()
+            return my_result["result"]
+        except Exception as e:
+            print(e)
+
+
+for transaction in get_data_transactions(path):
+    print(amount_transaction(transaction))
