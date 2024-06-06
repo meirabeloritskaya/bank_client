3bb13df43ad851ad1c182280dc1eaa05db582126
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-06-07 02:22:28 +0300

    return utils

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import json
+
+
+def get_data_transactions(path):
+    """функция возвращает список словарей с данными о  транзакциях"""
+    try:
+        with open(path, encoding="utf-8") as f:
+            try:
+                data_transactions = json.load(f)
+            except json.JSONDecodeError:
+                return []
+    except FileNotFoundError:
+        return []
+    return data_transactions
+
+
+# path = "C:/Users/Meira/PycharmProjects/card_client/data/operations.json"
