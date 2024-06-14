d31c32e2b65c3ddeb648c113bb75b331ec67f361
Liudmila Beloritskaya <meiramirth@gmail.com>
2024-06-14 13:04:49 +0300

    fix transactions.csv

--- src/read_transactions_excel.py
+++ src/read_transactions_excel.py
@@ -1,11 +1,10 @@
 import logging
 import csv
-
 import pandas as pd
 
 logger = logging.getLogger(__name__)
 file_handler = logging.FileHandler(
-    "C:/Users/Meira/PycharmProjects/card_client/logs/read_transactions_excel.xlsx.log",
+    "/logs/read_transactions_excel_xlsx.log",
     encoding="utf-8",
 )
 file_formatter = logging.Formatter(
@@ -19,23 +18,23 @@ logger.setLevel(logging.INFO)
 def get_data_transactions(path, n):
     """функция возвращает список словарей с данными о  транзакциях"""
     try:
-        logger.info("открытие файла transactions.excel.xlsx")
-        with open(path, encoding="utf-8") as f:
-            try:
+        logger.info("открытие файла transactions_excel.xlsx")
+        try:
                 logger.info("Получение информации о транзакциях")
-                reader = pd.read_csv(f, delimiter=";")
-                print(reader.shape)
-                print(reader.head(n))
+                df = pd.read_excel(path)
+                print(df.shape)
+                print(df.head(n))
 
-            except csv.Error as e:
-                logger.error(f"Ошибка чтения CSV файла: {e}")
-                return []
+
+        except pd.errors.ParserError as e:
+                logger.error(f"Ошибка при парсинге Excel файла: {e}")
+                return pd.DataFrame()
     except FileNotFoundError:
-        logger.error("путь к файлу transactions.excel.xlsx не найден")
-        return []
-    return reader
+        logger.error("путь к файлу transactions_excel.xlsx не найден")
+        return pd.DataFrame()
+    return df
 
 
 if __name__ == "__main__":
-    path = "C:/Users/Meira/PycharmProjects/card_client/data/transactions.excel.xlsx"
+    path = "C:/Users/Meira/PycharmProjects/card_client/data/transactions_excel.xlsx"
     list_trans = get_data_transactions(path, 6)
